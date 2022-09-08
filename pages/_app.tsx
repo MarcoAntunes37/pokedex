96fdfce1d04b2cf007d7cfcd5c40b8bc2dcd1138
Marco Aurelio <marcoantunesdev@gmail.com>
2022-09-08 18:16:12 -0300

    remove react query devtools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createTheme, ThemeProvider} from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App({ Component, pageProps }: AppProps) {
   const queryclient = new QueryClient();
@@ -29,7 +28,6 @@ function App({ Component, pageProps }: AppProps) {
         <CssBaseline/>
         <Component {...pageProps} />
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
